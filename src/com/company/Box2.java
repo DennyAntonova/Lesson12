dadf29c324ffe3ac6660a9d21d105070385fd79f
Denitsa Antonova <dennyantonova@gmail.com>
2021-11-18 20:01:15 +0200

    color

--- src/com/company/Box2.java
+++ src/com/company/Box2.java
@@ -1,2 +1,19 @@
-package com.company;public class Box2 {
+package com.company;
+
+public class Box2 {
+    String box2;
+    Color color;
+
+    public Box2(String box2, Color color) {
+        this.box2 = box2;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Box2{" +
+                "box2='" + box2 + '\'' +
+                ", color=" + color +
+                '}';
+    }
 }
